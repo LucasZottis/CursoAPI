33a8e0520ecb3d04a0244e8aeef52c281ba02863
LucasZottis <zottis.lucas@hotmail.com>
2021-12-22 06:37:18 -0300

    Criação das entidades.

---
+++ CursoAPI/CatalogoApi/Models/Categoria.cs
@@ -0,0 +1,9 @@
+﻿namespace CatalogoApi.Models
+{
+    public class Categoria
+    {
+        public int CodigoCategoria { get; set; }
+        public string Nome { get; set; }
+        public string Imagem { get; set; }
+    }
+}
---
+++ CursoAPI/CatalogoApi/Models/Produto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatalogoApi.Models
+{
+    public class Produto
+    {
+        public int CodigoProduto { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public string Imagem { get; set; }
+        public double Estoque { get; set; }
+        public DateTime DataCadastro { get; set; }
+    }
+}
