bbf7a594789b2b48232a70d2d99e44c3abc5bc58
LucasZottis <zottis.lucas@hotmail.com>
2021-12-30 00:00:12 -0300

    AJustes

--- CursoAPI/CatalogoApi/Models/Categoria.cs
+++ CursoAPI/CatalogoApi/Models/Categoria.cs
@@ -1,12 +1,25 @@
 ﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CatalogoApi.Models
 {
+    [Table("CATEGORIA")]
     public class Categoria
     {
+        [Key]
+        [Column("COD_CATEGORIA")]
         public int CodigoCategoria { get; set; }
+
+        [Required]
+        [Column("NOME")]
+        [MaxLength(80)]
         public string Nome { get; set; }
+
+        [Required]
+        [Column("URL_IMAGEM")]
+        [MaxLength(300)]
         public string Imagem { get; set; }
 
         public ICollection<Produto> Produtos { get; set; }
--- CursoAPI/CatalogoApi/Models/Produto.cs
+++ CursoAPI/CatalogoApi/Models/Produto.cs
@@ -1,21 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CatalogoApi.Models
 {
+    [Table("PRODUTO")]
     public class Produto
     {
+        [Key]
+        [Column("COD_PRODUTO")]
         public int CodigoProduto { get; set; }
+
+        [Required]
+        [Column("NOME_PRODUTO")]
+        [MaxLength(80)]
         public string Nome { get; set; }
+
+        [Required]
+        [Column("DES_PRODUTO")]
+        [MaxLength(300)]
         public string Descricao { get; set; }
+
+        [Required]
+        [Column("VAL_PRODUTO")]
         public decimal Preco { get; set; }
+
+        [Required]
+        [Column("URL_IMAGEM")]
+        [MaxLength(500)]
         public string Imagem { get; set; }
+
+        [Column("QUANTIDADE_ESTOUE")]
         public double Estoque { get; set; }
+
+        [Column("DATA_CADASTRO")]
         public DateTime DataCadastro { get; set; }
 
         public Categoria Categoria { get; set; }
+
         public int CodigoCategoria { get; set; }
     }
 }
